0990b54bb9aff2c2665a68a6e637b1077f1004ff
Aleksey Mudla <amudla@dstudio.software>
2025-05-06 00:11:23 +0400

    fix components exceed

--- src/PromomashTest/promomashtest.client/src/app/app-routing.module.ts
+++ src/PromomashTest/promomashtest.client/src/app/app-routing.module.ts
@@ -3,18 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { MainComponent } from './components/main/main.component';
 import { RegistrationComponent } from './components/registration/registration.component';
-import { StepFirstComponent } from './components/step-first/step-first.component';
-import { StepSecondComponent } from './components/step-second/step-second.component';
 
 const routes: Routes = [
   { path: '', component: MainComponent },
   {
     path: 'registration',
-    component: RegistrationComponent,
-    children: [
-      { path: 'step1', component: StepFirstComponent },
-      { path: 'step2', component: StepSecondComponent }
-    ]
+    component: RegistrationComponent
   }
 ];
 
